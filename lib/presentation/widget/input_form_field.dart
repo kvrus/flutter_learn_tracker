d0547a4471400599deafdfb684c4e28e3fc9b10b
VolkovaSvetlana <ssa.volkova@gmail.com>
2024-01-08 00:03:01 +0300

    text validator

--- lib/presentation/widget/input_form_field.dart
+++ lib/presentation/widget/input_form_field.dart
@@ -1,21 +1,31 @@
 import 'package:flutter/material.dart';
 
 class InputFormField extends StatelessWidget {
+  final GlobalKey<FormState> formKey;
   final TextEditingController controller;
   final String? hint;
 
-  const InputFormField({super.key, required this.controller, this.hint});
+  const InputFormField({super.key, required this.formKey, required this.controller, this.hint});
 
   @override
   Widget build(BuildContext context) {
-    return TextFormField(
-      controller: controller,
-      style: const TextStyle(
-        fontSize: 18,
-        fontWeight: FontWeight.w500,
-      ),
-      decoration: InputDecoration(
-        hintText: hint ?? "Enter your task name",
+    return Form(
+      key: formKey,
+      child: TextFormField(
+        controller: controller,
+        validator: (value) {
+          if (value == null || value.isEmpty) {
+            return 'Please enter some text';
+          }
+          return null;
+        },
+        style: const TextStyle(
+          fontSize: 18,
+          fontWeight: FontWeight.w500,
+        ),
+        decoration: InputDecoration(
+          hintText: hint ?? "Enter your task name",
+        ),
       ),
     );
   }
